10342263956ca5feeedadd20f12415ada232913a
sydschweber <sydney@brandeis.edu>
2016-04-21 14:38:32 -0400

    Rating class

---
+++ DataLayer/Rating.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Rating
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        public int BlogPostId { get; set; }
+
+        [ForeignKey("BlogPostId")]
+        public virtual BlogPost blogPost { get; set; }
+
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        public int ratingNumber { get; set; }
+    }
+}
