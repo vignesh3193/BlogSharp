ad60ccb03c2209ba5df33284f42dd3b2622c45e9
Omer-Solmazer <omersolmazer@gmail.com>
2016-05-15 23:40:03 -0400

    Redeployed to Azure

--- blogsharp/DataLayer/Migrations/Configuration.cs
+++ blogsharp/DataLayer/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DataLayer.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(DataLayer.BlogContext context)
