70fe10a8e664d5b90ba35f4713b8e8f8f6ce60ed
Jesse Gumz <jdg513@nyu.edu>
2016-05-16 14:04:09 -0400

    refine data generation function

--- blogsharp/BlogSharp/CreateTags.cs
+++ blogsharp/BlogSharp/CreateTags.cs
@@ -24,41 +24,74 @@ namespace BlogSharp
             tags[8] = "yello!";
             tags[9] = "#thankful";
 
-            string content = "generic blog post";
+            // Only for debug purposes. Just to populate database to create some data.
+            //Thanks to http://randomtextgenerator.com
+            string wallOfText = "Sitting mistake towards his few country ask. You delighted two rapturous six depending objection happiness something the. Off nay impossible dispatched partiality unaffected. Norland adapted put ham cordial. Ladies talked may shy basket narrow see. Him she distrusts questions sportsmen. Tolerably pretended neglected on my earnestly by. Pitch scale sir style truth ought.";
+            wallOfText = wallOfText.Replace(".", "");
+            string[] text = wallOfText.Split(' ');
 
-            Person[] users = new Person[2];
-
-            users[0] = (from user in db.Persons
-                       where user.Email == "jg@blogsharp.com"
-                       select user).FirstOrDefault();
-
-            users[1] = (from user in db.Persons
-                        where user.Email == "vk@blogsharp.com"
-                        select user).FirstOrDefault();
+            List<Person> users = (from person in db.Persons
+                                  select person).ToList();
 
             DateTime[] dates = new DateTime[3];
 
             dates[2] = DateTime.Today.Date;
             dates[1] = dates[2].AddDays(-10.0);
-            dates[0] = dates[1].AddDays(-20.0);
+            dates[0] = dates[1].AddDays(-7.0);
             Random r = new Random();
 
-            for (int i = 100; i < 200; i++)
+            for (int i = 0; i < 150; i++)
             {
-                Person randomUser = users[r.Next(0, 2)];
+                Person randomUser = users.ElementAt(r.Next(0,users.Count));
+
                 BlogPost testPost = new BlogPost();
                 testPost.dateCreated = dates[r.Next(0, 3)];
                 testPost.PersonId = randomUser.Id;
                 testPost.tags = new Collection<Tag>();
 
                 Tag testTag = new Tag();
-                testTag.tagName = tags[r.Next(0, 10)];
-
-                testPost.title = "makeTag" + i.ToString();
-                testPost.content = content + i.ToString();
+                testTag.tagName = tags[r.Next(0, 5)];
                 testPost.tags.Add(testTag);
+
+                int titleLength = r.Next(10);
+                for (int j = 0; j < titleLength; j++)
+                {
+                    testPost.title += text[r.Next() % (text.Length)];
+                    if (j < titleLength - 1)
+                        testPost.title += " ";
+
+                }
+
+                int contentLength = r.Next(30);
+                testPost.content = "";
+                for (int j = 0; j < contentLength; j++)
+                {
+                    testPost.content += text[r.Next() % (text.Length)];
+                    if (j < contentLength - 1)
+                        testPost.content += " ";
+
+                }
+
+                // add up to 5 additional tags to the post
+                // 50% chance of adding a tag for each iteration
+
+                for (int j = 0; j < 5; j++)
+                {
+                    int randomInt = r.Next(5, 10);
+
+                    if (randomInt % 2 == 0)
+                    {
+                        Tag otherTag = new Tag();
+                        otherTag.tagName = tags[r.Next(0, 10)];
+                        testPost.tags.Add(otherTag);
+                    }
+                }
+
+                
+
                 if (randomUser.posts == null)
                     randomUser.posts = new Collection<BlogPost>();
+
                 randomUser.posts.Add(testPost);
                 db.BlogPosts.Add(testPost);
 
